d127d6f0f32d4f83d591c0bb7711c7f728b555db
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-15 17:59:19 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,16 @@
-# Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(phrase)
+  # define the method 
+  if phrase == 'i love you grandma!'.upcase
+    # use a double == to set it equal 
+    # whats the code saying? 
+    return "i love you too pumpkin!".upcase
+  end 
+   # its saying if the phrase(input from the spec) is equal to this exact line then return this and end the method. if not then go to next if. 
+  if phrase != phrase.upcase()
+    return "huh?! speak up, sonny!".upcase
+    # code exp: if phrase is not all upcase then return this
+  else 
+   return 'no, not since 1938!'.upcase
+  end 
+  # code exp: if it doesnt match the previous condtions always return this.
+end 

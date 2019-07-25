07549f420796898b09981a7d3a06ee0a2e2d84ee
Darin Webster <darinleenavy@gmail.com>
2019-07-25 21:29:32 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,33 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(respond)
+  respond = "I LOVE YOU TOO PUMPKIN!"
+end
+
+def speak_to_grandma(greeting)
+  response = "HUH?! SPEAK UP, SONNY!"
+  case greeting
+  when "I LOVE YOU GRANDMA!"
+    response = 'I LOVE YOU TOO PUMPKIN!'
+  when 'WHAT DID YOU EAT TODAY?'
+    response = "NO, NOT SINCE 1938!"
+  when 'WHAT?'
+    response = "NO, NOT SINCE 1938!"
+  end
+  
+  response
+end
+
+# def speak_to_grandma(speak)
+#   speak = 'WHAT DID YOU EAT TODAY?'
+#   if speak == "WHAT DID YOU EAT TODAY?"
+#     puts "NO, NOT SINCE 1938!"
+#   end
+# end
+
+
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
